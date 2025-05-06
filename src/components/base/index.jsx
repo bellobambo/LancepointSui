81fd6d31bcdf947ed896afd070dba648b60fe1c3
Teejay012 <tijesunimioluwakoya@gmail.com>
2025-05-06 16:32:50 +0100

    BG Updated

--- src/components/base/index.jsx
+++ src/components/base/index.jsx
@@ -1,12 +1,13 @@
 import Navbar from "../Navbar";
 import Sidebar from "../Sidebar";
+import "@/app/globals.css";
 
 export default function Base({ children }) {
   return (
     <>
       <div className="w-full h-screen flex ">
         <Sidebar />
-        <div className="w-full h-full overflow-y-auto overflow-x-hidden relative">
+        <div className="w-full h-full overflow-y-auto overflow-x-hidden relative bg-image">
           <Navbar />
           {children}
         </div>
